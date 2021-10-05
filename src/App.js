7467753697b65bbdfdfe39c8a2ec538b822af79f
Nicole WItherall <nicwitherall@gmail.com>
2021-10-05 15:47:45 -0400

    set up function to find if mobile view, set up sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import "./styles/style.css"
-import { useState } from "react"
-// import components
+import { useState, useEffect } from "react"
+// COMPONENTS
 import Picker from "./components/Picker"
 import Player from "./components/Player"
-// imports from assets:
-// 1. images
+// ASSETS:
+// images
 import lightModeImg from "./assets/images/light.png"
 import darkModeImg from "./assets/images/dark.png"
 
 
 const App = () => {
 
+  // STATE
   const [isMobile, setIsMobile] = useState(false)
   const [isDarkMode, setIsDarkMode] = useState(false)
+
+  //  FUNCTIONS
+  // check if being viewed on mobile device and assign to state
+  useEffect(() => {
+      if (window.innerWidth <= 1024) {
+        setIsMobile(true)
+      } else {
+        setIsMobile(false)
+      }
+  }, [])
 
   return (
     <div className="App">
       <nav>
         <h1>Calmly</h1>
-        <p tabIndex="0">dark/light here</p>
+        <img src={`${isDarkMode === false ? (darkModeImg) : (lightModeImg)}`} tabIndex="0" alt="toggle dark mode" />
       </nav>
       <main>
         <Picker />
